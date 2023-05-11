93b3bfce0c942d48f0997f7fe2c4d1a47097e96a
Déborah Dobles <126917588+deborahdobles@users.noreply.github.com>
2023-05-11 02:17:22 +0000

    final commit with bonus

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,33 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  const [selectedLight, setSelectedLight] = useState("");
+  const [selectedLight, setSelectedLight] = useState("red");
+  const [showPurple, setShowPurple] = useState(false);
 
-  const redLightClassNames = ["light", "red"];
+  useEffect(() => {
+    const interval = setInterval(cycleLight, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [selectedLight]);
+
+  const cycleLight = () => {
+    if (selectedLight === "red") {
+      setSelectedLight("yellow");
+    } else if (selectedLight === "yellow") {
+      setSelectedLight("green");
+    } else if (selectedLight === "green") {
+      setSelectedLight(showPurple ? "purple" : "red");
+    } else if (selectedLight === "purple") {
+      setSelectedLight("red");
+    }
+  };
+
+  const togglePurple = () => {
+    setShowPurple((prevState) => !prevState);
+  };
+
+  let redLightClass = "light red";
+  let yellowLightClass = "light yellow";
+  let greenLightClass = "light green";
+  let purpleLightClass = "light purple";
+
   if (selectedLight === "red") {
-    redLightClassNames.push("active");
+    redLightClass += " active";
   }
-
-  const yellowLightClassNames = ["light", "yellow"];
   if (selectedLight === "yellow") {
-    yellowLightClassNames.push("active");
+    yellowLightClass += " active";
   }
-
-  const greenLightClassNames = ["light", "green"];
   if (selectedLight === "green") {
-    greenLightClassNames.push("active");
+    greenLightClass += " active";
+  }
+  if (selectedLight === "purple") {
+    purpleLightClass += " active";
   }
 
   return (
     <div className="text-center">
-	  <div className="traffic-hanger"></div>
+      <div className="traffic-hanger"></div>
       <div className="traffic-light">
-        <div className={redLightClassNames.join(" ")} onClick={() => setSelectedLight("red")}></div>
-        <div className={yellowLightClassNames.join(" ")} onClick={() => setSelectedLight("yellow")}></div>
-        <div className={greenLightClassNames.join(" ")} onClick={() => setSelectedLight("green")}></div>
+        <div className={redLightClass}></div>
+        <div className={yellowLightClass}></div>
+        <div className={greenLightClass}></div>
+        {showPurple && <div className={purpleLightClass}></div>}
       </div>
+      <button onClick={togglePurple}>
+        {showPurple ? "Hide Purple" : "Include Purple"}
+      </button>
     </div>
   );
 };
 
 export default Home;
+
+
